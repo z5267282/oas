eabfd43b12df0c7b41d4ff4c87ad96f602af13ed
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-09-20 00:02:03 +0800

    tiktok fe

---
+++ tiktok-fe-2024/question.md
@@ -0,0 +1,2 @@
+Given a string of 100 lowercase English alphabet characters, find the longest substring without repeated characters.
+For example "abca" would give back 3 with substirng "abc".
---
+++ tiktok-fe-2024/sol.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cassert>
+#include <string>
+#include <vector>
+
+auto solve(std::string s) -> int;
+
+auto main(void) -> int {
+    assert(solve("abcde") == 5);
+    assert(solve("abca") == 3);
+
+    return 0;
+}
+
+auto solve(std::string s) -> int {
+    // look at all substring lengths starting from s' size
+    // for each substring length, use a contiguous-sized sliding window to check all substrings to see if they are valid
+    for (auto length = s.size(); length > 0; ++length) {
+        // initialise the first window
+        auto window = std::vector<int>(26, 0);
+        auto high = 0;
+        for (auto i = std::size_t{0}; i < length; ++i) {
+            auto pos = s.at(i) - 'a';
+            window.at(pos) += 1;
+            high = std::max(high, window.at(pos));
+        }
+        // should be impossible to be 0
+        if (high == 1) {
+            return length;
+        }
+        for (auto i = length; i < s.size(); ++i) {
+            auto loss = s.at(i - length) - 'a';
+            auto gain = s.at(i) - 'a';
+            window.at(loss) -= 1;
+            window.at(gain) += 1;
+            high = std::max(high, std::max(window.at(loss), window.at(gain)));
+            if (high == 1) {
+                return length;
+            }
+        }
+    }
+    return -1;
+}
